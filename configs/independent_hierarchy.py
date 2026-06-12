3f7fc379954c1d803b3b18ee9b7d9f6aeb79a8a8
Aleks <s234828@dtu.dk>
2026-06-12 14:37:11 +0200

    epoch reduction

--- configs/independent_hierarchy.py
+++ configs/independent_hierarchy.py
@@ -26,7 +26,7 @@ CONFIG = {
     # training
     "train_size": 0.8,   # Only used if dataset doesn't have official train/test split
     "test_size": 0.2,    # Only used if dataset doesn't have official train/test split
-    "epochs": 80,
+    "epochs": 40,
     "learning_rate": 1e-5,
     "weight_decay": 1e-4, # Adam = 0, AdamW = 1e-2
     "batch_size": 4,
